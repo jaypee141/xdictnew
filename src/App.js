7049a76e4496111dc9eabf48ec6098b8a9f82eb9
unknown <jaypee141@gmail.com>
2024-03-02 11:44:06 +0530

    gooooo

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import "./App.css";
+import React, { useState } from "react";
+
+const dict = [
+  {
+    word: "React",
+    meaning: "A JavaScript library for building user interfaces.",
+  },
+
+  { word: "Component", meaning: "A reusable building block in React." },
+
+  { word: "State", meaning: "An object that stores data for a component." },
+];
+
+function App() {
+  const [data, setdata] = useState("");
+  const [refer, setRefer] = useState("");
+
+  // console.log(dict[0]);
+  const findInput =(e)=>{
+    setdata(e.target.value);
+  }
+  // let newData=()=>{
+
+  // }
+  let data1=[];
+
+  const getData = () => {
+    const words = data.toLowerCase();
+    const findWord = dict.find(
+      (entry) => entry.word.toLowerCase() === words
+    );
+    if (findWord) {
+      setRefer(findWord.meaning);
+      setdata("");
+    } else {
+      setRefer("Word not found in the dict.");
+    }
+  };
+  return (
+    <div className="App">
+      <h1>Dictionary App</h1>
+      <input
+        value={data}
+        type="text"
+        placeholder="Search for a word..."
+        onChange={findInput}
+      />
+      <button onClick={getData}>Search</button>
+      <p>Definition:</p>
+      <p>{refer}</p>
+    </div>
+  );
+}
+
+export default App;
